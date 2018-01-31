448e53e85d15b495eac44bfab2b1d87080e2a54c
agnes <agnes@dyn-184-171-77-151.uoregon.edu>
2018-01-31 10:43:15 -0800

    changed output and resource paths to avoid manual updating

--- runROC_R1beh.m
+++ runROC_R1beh.m
@@ -10,9 +10,8 @@ expt.experiment_code='DEV'; %Replace this with a 3-4 letter code for your experi
 expt.experiment_notes='This is code to run the DEV ROC Task R1 beh version'; %Add any pertinent notes here
 expt.script_revision_date='12/13/2017'; %Replace with the date you last revised your script
 expt.tdfile='ROC_R1beh.txt'; %Replace with the name of your td file
-expt.resource_path='/Users/danicosme/Documents/code/sanlab/ROC/Resources'; %Replace with the path for your resource folder
-expt.output_folder='/Users/danicosme/Documents/code/sanlab/ROC/Output'; %Replace with the path for your output folder
-expt.default_start='scan_start.jpg'; %Replace if you'd like to use an image other than default_start.jpg
+expt.resource_path='Resources'; %Replace with the path for your resource folder
+expt.output_folder='Output'; %Replace with the path for your output folderexpt.default_start='scan_start.jpg'; %Replace if you'd like to use an image other than default_start.jpg
 expt.kid_start='kid_start.jpg'; %Replace if you'd like to use an image other than kid_start.jpg
 expt.startToggle=0; %Default is 0; use 1 for a user-specified version
 
